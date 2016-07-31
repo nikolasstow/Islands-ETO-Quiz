985d9ccaa7b5a40eb81039f4b90bceb87ec08d19
Nikolas Stow <nikolasstow@gmail.com>
2016-07-31 11:32:41 -0700

    - Finished switching to object oriented code

--- functions.js
+++ functions.js
@@ -2,10 +2,43 @@ var ingredients = [], difficulty;
 
 for(i = 0; ingredientsArray.length > i; i++) ingredients[ingredientsArray[i][0]] = ingredientsArray[i][1];
 
-$('.difficulty').on('click','button', function () {
-	difficulty = $(this).val();
-	$('.difficulty').hide();
-	easyMode();
+var Quiz = function (array) {
+    this.title = array.title; // Question Title
+    this.amountOfQuestions = array.amountOfQuestions; // Amount of Questions
+
+    this.questionPosition = 0; // Current question number
+    this.amountCorrect = 0; // Amount of correct answers
+    this.percent = 1; // Percentage of correct answers
+    this.correctAnswer = "";
+
+    this.questions = array.questions;
+    this.question = function () { array.question(this); }
+    this.finish = function () { array.finish(this); }
+    this.checkAnswer = function (data) { array.checkAnswer(this, data); }
+
+    this.start = function () {
+        if(array.before) array.before(); // Code to run before starting quiz
+
+        $('.question .title').text(this.title); // Insert title into page
+        $('.question').show(); // Show question
+
+        if(this.question) this.question(this); // Start first question
+        this.updateStatus();
+    }
+
+    this.updateStatus = function () {
+        $('.status .questions').text(this.questionPosition + "/" + this.amountOfQuestions + " ");
+        $('.status .percent').text("[" + Math.round(this.percent * 100) + "%]");
+    }
+
+}
+
+$(document).ready(function () {
+    $('.difficulty').on('click','button', function () {
+        difficulty = $(this).val();
+        $('.difficulty').hide();
+        EASY.start();
+    });
 });
 
 function listIngredients(data) {
@@ -14,6 +47,14 @@ function listIngredients(data) {
 	for(i = 0; i < data.length; i++) html.push('<li>' + ingredients[data[i][0]] + (data[i][1] ? " (If Ordered)</li>" : "</li>"))
 	html.push('</ul>');
 	return html.join("");
+}
+
+function inArray(array, item) {
+    var inArray = false;
+    for(i = 0; i < array.length; i++) {
+        if(array[i] == item) inArray = true;
+    }
+    return inArray;
 }
 
 function shuffle(array) {
--- mode/easy.js
+++ mode/easy.js
@@ -1,141 +1,66 @@
-var Quiz = function (array) {
-    this.title = array.title; // Question Title
-    this.amountOfQuestions = array.amountOfQuestions; // Amount of Questions
-
-    this.questionPosition = 0; // Current question number
-    this.amountCorrect = 0; // Amount of correct answers
-    this.percent = 1; // Percentage of correct answers
-
-    this.questions = array.questions;
-
-    this.currentQuestion = this.questions[this.questionPosition];
-
-    this.start = function () {
-    	if(array.before) array.before(); // Code to run before starting quiz
-
-    	$('.question .title').text(this.title); // Insert title into page
-		$('.question').show(); // Show question
-
-		if(array.question) array.question(this); // Start first question
-		this.updateStatus();
-    }
-
-    this.updateStatus = function () {
-    	$('.status .questions').text(this.questionPosition + "/" + this.amountOfQuestions + " ");
-    }
-
-}
-
-var easyAnswer = "";
-var sortedMenu = [];
-
-function shuffle(array) {
-    let counter = array.length;
-
-    // While there are elements in the array
-    while (counter > 0) {
-        // Pick a random index
-        let index = Math.floor(Math.random() * counter);
-
-        // Decrease counter by 1
-        counter--;
-
-        // And swap the last element with it
-        let temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
-    }
-
-    return array;
-}
+var AMOUNT_OF_QUESTIONS = 5;
+var MAX_ANSWERS = 5;
 
 var EASY = new Quiz({
 	title: "Match the menu item to the ingredients",
-	amountOfQuestions: 15,
-	questions: shuffle(menu).slice(0,this.amountOfQuestions),
+	amountOfQuestions: AMOUNT_OF_QUESTIONS,
+	questions: shuffle(menu).slice(0,AMOUNT_OF_QUESTIONS),
 	before: function () {
 		// Sort Menu into categories
+		EASY.sortedMenu = [];
 		for(i = 0; i < menu.length; i++) {
 			var curItem = menu[i];
-			if(!sortedMenu[curItem.cat]) sortedMenu[curItem.cat] = [];
-			sortedMenu[curItem.cat].push(curItem.itemName);
+			if(!EASY.sortedMenu[curItem.cat]) EASY.sortedMenu[curItem.cat] = [];
+			EASY.sortedMenu[curItem.cat].push(curItem.itemName);
 		}
 	},
 	question: function (quiz) {
-		var item = quiz.currentQuestion; // Current question
-		var answers = getAnswers(item , 5); // Get answers
+		var item = quiz.questions[quiz.questionPosition]; // Current question
 
-		easyAnswer = item.itemName;
+		var answers = quiz.sortedMenu[item.cat]; // Get all items in current category
+		var curLoc = answers.indexOf(item.itemName); // Get location of correct item in array
+		answers.splice(curLoc,1); // Remove current item before shuffling
+		answers = shuffle(answers).slice(0, (MAX_ANSWERS - 1)); // Shuffling items, and selecting one less than the max
+		answers.push(item.itemName); // Re-adding current item
+		answers = shuffle(answers); // Re-shuffle answers
 
-		$('.info').html(listIngredients(item.ingredients));
-		$('.answers').html(listAnswers(answers));
+		quiz.correctAnswer = item.itemName;
+
+		// List Ingredients
+		var ingredientsHTML = [];
+		ingredientsHTML.push('<ul>');
+		for(i = 0; i < item.ingredients.length; i++) ingredientsHTML.push('<li>' + ingredients[item.ingredients[i][0]] + (item.ingredients[i][1] ? " (If Ordered)</li>" : "</li>"))
+		ingredientsHTML.push('</ul>');
+		$('.info').html(ingredientsHTML.join(""));
+
+		// List Answers
+		var answersHTML = [];
+		for(i = 0; i < answers.length; i++) answersHTML.push('<input type="radio" name="answer" class="answer" value="' + answers[i] + '"> ' + answers[i] + '<br />');
+		$('.answers').html(answersHTML.join(""));
 	},
-	checkAnswer: function (data) {
-		checkAnswer(data);
+	checkAnswer: function (quiz, data) {
+		if(data == quiz.correctAnswer) { // If answer is correct
+			quiz.amountCorrect++;
+			$('.status .correct').html('<font color="green">Correct! </font>');
+		} else $('.status .correct').html('<font color="red">Incorrect (It was ' + quiz.correctAnswer + ')</font>');
+
+			quiz.questionPosition++; // Move to next question
+			quiz.percent = quiz.amountCorrect/ quiz.questionPosition; // update percent correct
+			quiz.updateStatus();
+
+			if(quiz.questionPosition < quiz.amountOfQuestions) quiz.question();
+			else quiz.finish();
+	},
+	finish: function (quiz) {
+		$('.question, .status').hide(); // Hide question and status
+		$('.difficulty, .results').show(); // show results
+
+		$('.results').text('Congrats You Got: ' + Math.round(quiz.percent * 100) + "% Would you like to try again?"); // Results text
 	}
 });
 
 $(document).ready(function () {
 	$('.answers').on('click', '.answer', function () {
-		checkAnswer($(this).val());
+		EASY.checkAnswer($(this).val());
 	});
 });
-
-function easyMode() {
-	EASY.start();
-}
-
-function newQuestion() {
-
-	var item = EASY.questions[EASY.questionPosition];
-	var answers = getAnswers(item , 5);
-
-	easyAnswer = item.itemName;
-
-	$('.info').html(listIngredients(item.ingredients));
-	$('.answers').html(listAnswers(answers));
-}
-
-function getAnswers (realItem, maxNum) {
-	var answers = sortedMenu[realItem.cat]; // Get all items in current category
-	var curLoc = answers.indexOf(realItem.itemName); // Get location of correct item in array
-	answers.splice(curLoc,1); // Remove current item before shuffling
-	answers = shuffle(answers).slice(0, (maxNum - 1)); // Shuffling items, and selecting one less than the max
-	answers.push(realItem.itemName); // Re-adding current item
-	return shuffle(answers); // shuffling and returning answers
-}
-
-function inArray(array, item) {
-	var inArray = false;
-	for(i = 0; i < array.length; i++) {
-		if(array[i] == item) inArray = true;
-	}
-	return inArray;
-}
-
-function listAnswers (data) {
-	var html = [];
-	for(i = 0; i < data.length; i++) html.push('<input type="radio" name="answer" class="answer" value="' + data[i] + '"> ' + data[i] + '<br />');
-	return html.join("");
-}
-
-function checkAnswer(data) {
-	if(data == easyAnswer) {
-		EASY.amountCorrect++;
-		$('.status .correct').html('<font color="green">Correct! </font>');
-	} else $('.status .correct').html('<font color="red">Incorrect (It was ' + easyAnswer + ')</font>');
-
-		EASY.questionPosition++;
-		EASY.percent = EASY.amountCorrect/ EASY.questionPosition;
-		$('.status .percent').text("[" + Math.round(EASY.percent * 100) + "%]");
-
-		if(EASY.questionPosition < EASY.amountOfQuestions) newQuestion();
-		else finishQuiz();
-}
-
-function finishQuiz () {
-	$('.question, .status').hide();
-	$('.difficulty, .results').show();
-
-	$('.results').text('Congrats You Got: ' + Math.round(EASY.percent * 100) + "% Would you like to try again?");
-}
